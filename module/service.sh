2d468415792948e5d55b0546096554c048bd4ff3
Ziad <ziad.ahmed7881@gmail.com>
2026-01-25 01:41:07 +0200

    update module to v1.9 (76)
    # Commit message
    * Removed the `p2p` and `wpa_supplicant` overlay files
    * Added tweaks for lowmemorykiller (these tweaks greatly improved the overall responsiveness of the system)
    * Some SELinux fixes

--- module/service.sh
+++ module/service.sh
@@ -1,6 +1,14 @@
 #!/system/bin/sh
+# modify kernel swappiness & make /system/bin/sswap binary executable.
 echo 160 > /proc/sys/vm/swappiness
 chmod 775 /system/bin/sswap
+# create a zram disk with 2048 (2GB) size
+# (this is currently the best zram size as far as my tests have gone).
 /system/bin/sswap -s -z -f 2048
+# tweak low memory killer
+# but first we must wait for sys.boot_completed signal
+while [ "$(getprop sys.boot_completed)" != "1" ]; do sleep 1; done
+# at this stage sys.boot_completed signal must've been sent, we can set lmk minfree params
+echo "2560,12800,25600,99840,115200,131072" > /sys/module/lowmemorykiller/parameters/minfree
 # will automatically grant permission on every single boot.
 pm grant ziad_mrx.samsung.incall_audio.ds.svc android.permission.READ_PHONE_STATE
